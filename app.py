06b8d00e9920d6efa540286f98f10246f642a987
Alexander Hoffmann <Hoff@Alexanders-MacBook-Pro.local>
2017-03-31 11:15:27 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    
+    if req.get("result").get("action") == "read-recipe":
+        speech = "You wanted to read the recipe. Unfortunately, I forgot it."
+        
+    else if req.get("result").get("action") == "add-ingredient":
+        #do another
+        if urlopen("http://www.bbc.com/news").read().find("Singapore") == -1:
+            speech="No news of Singapore on BBC News front page"
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+            # "data": data,
+            # "contextOut": [],
+            #"source": #application name
+        }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
