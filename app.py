a66a630a0f6fad8c805fc02783032f25e3f2d1b4
a-hoffmann <alexandr.hoffmann@gmail.com>
2017-06-06 14:35:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,8 @@ def recordfeedback(param):
     url = 'https://script.google.com/macros/s/AKfycbzxO9ACRxnerMMWkNruSAue_MHdxKAE_r193bRcUlQhK87mxEf5/exec'
     post_fields = {'sample': param.get("number"),
                   'feedback': param.get("any")}
+    print(post_fields['sample'])
+    print(post_fields['feedback'])
     print(json.dumps(post_fields))
     request = Request(url, json.dumps(post_fields))
     request.add_header('Content-Type', 'application/json')
